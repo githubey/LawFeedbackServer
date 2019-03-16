3757e3ad732a1b85658660516603a8ffbbdd0e8f
jiho <jihojihoya02@gmail.com>
2019-03-16 20:09:20 +0900

    first commit

---
+++ src/main/java/com/codefair/lawfeedback/LawfeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.codefair.lawfeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawfeedbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawfeedbackApplication.class, args);
+    }
+
+}
