a60e2a6409e778acd123c62c7a87f47ec3b2486f
jiho <jihojihoya02@gmail.com>
2019-05-15 22:58:06 +0900

    글쓰기 기능에서 사용하는 service, controller 생성

--- src/main/java/com/codefair/lawfeedback/service/ArticleService.java
+++ src/main/java/com/codefair/lawfeedback/service/ArticleService.java
@@ -1,12 +1,17 @@
 package com.codefair.lawfeedback.service;
 
 import com.codefair.lawfeedback.controller.article.ArticleListItemTO;
+import com.codefair.lawfeedback.controller.article.WriteArticleTO;
+import com.codefair.lawfeedback.domain.Article;
+import com.codefair.lawfeedback.domain.RelatedJob;
 import com.codefair.lawfeedback.repository.ArticleRepository;
+import com.codefair.lawfeedback.repository.RelatedJobRepository;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,9 +21,25 @@ public class ArticleService {
 
     private static final Logger logger = LoggerFactory.getLogger("ArticleService");
     private final ArticleRepository articleRepository;
+    private final RelatedJobRepository relatedJobRepository;
 
     public List<ArticleListItemTO> getArticleList() {
         return articleRepository.findAllByOrderByIdDesc()
                 .stream().map(ArticleListItemTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void writeArticle(WriteArticleTO writeArticleTO) {
+        Article article = Article.builder()
+                .userId(writeArticleTO.getUserId())
+                .title(writeArticleTO.getTitle())
+                .content(writeArticleTO.getContent()).build();
+        article = articleRepository.save(article);
+        LoggerFactory.getLogger("ArticleService").info("" + article.getId());
+
+        for (Long jobId : writeArticleTO.getJobIdList()) {
+            RelatedJob relatedJob = new RelatedJob(jobId, article.getId());
+            relatedJobRepository.save(relatedJob);
+        }
     }
 }
