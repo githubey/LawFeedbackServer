2675a83394d110fb49d62a0b30064c4c552a2e3a
jiho <jihojihoya02@gmail.com>
2019-05-15 22:53:39 +0900

    primary key 자동생성 규칙 변경(각 테이블별로 증가하도록)

--- src/main/java/com/codefair/lawfeedback/domain/Job.java
+++ src/main/java/com/codefair/lawfeedback/domain/Job.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class Job {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, columnDefinition = "varchar(50)", nullable = false)
--- src/main/java/com/codefair/lawfeedback/domain/User.java
+++ src/main/java/com/codefair/lawfeedback/domain/User.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, columnDefinition = "varchar(50)", nullable = false)
